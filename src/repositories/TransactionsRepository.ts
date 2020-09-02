65fb8db939dd81ab694360d11c1c751c8935a3bb
Newton Duarte <newton_@hotmail.com>
2020-09-02 05:49:09 -0300

    TransactionRepository, CreateTransactionService and transaction.routes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acc, cur): number => {
+      if (cur.type === 'income') {
+        acc += cur.value;
+      }
+
+      return acc;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acc, cur): number => {
+      if (cur.type === 'outcome') {
+        acc += cur.value;
+      }
+
+      return acc;
+    }, 0);
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome
+    };
+    
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value}: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
